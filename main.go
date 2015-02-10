c3658080a1771415775397b17b98c12395da1bae
rekby <timofey@koolin.ru>
2015-02-10 12:41:28 +0300

    gofmt

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-    "os"
-    "github.com/rekby/multiex"
+	"github.com/rekby/multiex"
+	"os"
 )
 
-func f1(){
-    fmt.Println("Func 1")
-    fmt.Println(os.Args)
+func f1() {
+	fmt.Println("Func 1")
+	fmt.Println(os.Args)
 }
 
-func f2(){
-    fmt.Println("Func 2")
-    fmt.Println(os.Args)
+func f2() {
+	fmt.Println("Func 2")
+	fmt.Println(os.Args)
 }
 
 func main() {
-    multiex.Register(multiex.ExecutorDescribe{Name: "test-f1", Function: f1})
-    multiex.Register(multiex.ExecutorDescribe{Name: "test-f2", Function: f2})
-    multiex.Main()
+	multiex.Register(multiex.ExecutorDescribe{Name: "test-f1", Function: f1})
+	multiex.Register(multiex.ExecutorDescribe{Name: "test-f2", Function: f2})
+	multiex.Main()
 }
